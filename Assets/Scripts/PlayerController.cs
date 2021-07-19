4c719ab5fba0493234946db96ee4215a3b1c889a
amitbonty <71116433+amitbonty@users.noreply.github.com>
2021-07-19 20:49:31 +0530

    Added Player controller for animations and flipping character.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    public void Update()
+    {
+        PlayerFlip();
+    }
+    public void PlayerFlip()
+    {
+        float moveSpeed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("MoveSpeed", Mathf.Abs(moveSpeed));
+        Vector3 scale = transform.localScale;
+        if (moveSpeed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (moveSpeed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
