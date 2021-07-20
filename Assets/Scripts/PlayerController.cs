b78f17ef5cd03ff7eb033582ec0f8a5fd996e7af
amitbonty <71116433+amitbonty@users.noreply.github.com>
2021-07-20 21:34:18 +0530

    Feature 3 - Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    private Rigidbody2D rb;
+    public float jump;
+
+
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
     public void Update()
     {
-        PlayerFlip();
-    }
-    public void PlayerFlip()
-    {
         float moveSpeed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        PlayerFlip(moveSpeed);
+        PlayerMovement(moveSpeed, vertical);
+        PlayerJump(vertical);
+    }
+    public void PlayerMovement(float moveSpeed, float vertical)
+    {
+        //move player horizontally
+        Vector3 position = transform.position;
+        position.x += moveSpeed * speed * Time.deltaTime;
+        transform.position = position;
+
+        //move player vertically
+        if (vertical > 0)
+        {
+            rb.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+    }
+    public void PlayerFlip(float moveSpeed)
+    {
+       
         animator.SetFloat("MoveSpeed", Mathf.Abs(moveSpeed));
         Vector3 scale = transform.localScale;
         if (moveSpeed < 0)
@@ -23,5 +51,16 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+    }
+    public void PlayerJump(float vertical)
+    {
+        if(vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
