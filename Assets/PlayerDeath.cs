2992dd10c8f66314d626c88ff5f4e612d96aac9b
amitbonty <71116433+amitbonty@users.noreply.github.com>
2021-07-21 21:42:41 +0530

    Feature4-TileMap Setup

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player Death!");
+            SceneManager.LoadScene("2DPlatformerLevel1");
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,27 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     public float jump;
     public bool isGrounded;
-
-
-
+    public float vertical;
 
     private void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+       
     }
 
     public void Update()
     {
         float moveSpeed = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Jump");
+        vertical = Input.GetAxisRaw("Jump");
+
         PlayerFlip(moveSpeed);
         PlayerMovement(moveSpeed, vertical);
         PlayerJump(vertical);
         PlayerCrouch();
+    }
+    public void FixedUpdate()
+    {
+       
     }
     public void PlayerMovement(float moveSpeed, float vertical)
     {
@@ -42,7 +46,6 @@ public class PlayerController : MonoBehaviour
     }
     public void PlayerFlip(float moveSpeed)
     {
-       
         animator.SetFloat("MoveSpeed", Mathf.Abs(moveSpeed));
         Vector3 scale = transform.localScale;
         if (moveSpeed < 0)
@@ -57,29 +60,10 @@ public class PlayerController : MonoBehaviour
     }
     public void PlayerJump(float vertical)
     {
-        
-      
-            if (Input.GetKey(KeyCode.Space))
-            {
-               animator.SetBool("Jump", true);
-               //animation.Play("PlayerJump");
-            
-            }
-            else
-            {
-                animator.SetBool("Jump", false);
-            }
-        
+      animator.SetBool("Jump", Input.GetKey(KeyCode.Space));
     }
     public void PlayerCrouch()
     {
-        if(Input.GetKey(KeyCode.C))
-            {
-              animator.SetBool("Crouch", true);
-            }
-        else
-          {
-            animator.SetBool("Crouch", false);
-          }
+      animator.SetBool("Crouch", Input.GetKey(KeyCode.C));
     }
 }
